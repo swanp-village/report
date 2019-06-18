e264d14565e51d44b62b32a7df1cce2f0f41954a
Yudai Udagawa <nimius.rd@gmail.com>
2019-06-18 19:19:59 +0900

    Update

---
+++ config/two_same.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+config = {
+    'eta': 1,  # 結合損
+    'a': np.array([1, 1]),  # 減衰定数
+    'K': np.array([0.65, 0.17, 0.43]),  # 結合効率
+    'L': np.array([2.284e-4, 2.284e-4]),  # リング周長
+    'n': 3.3938,  # 屈折率
+    'lambda': np.arange(1545e-9, 1555e-9, 1e-12)  # 波長レンジ
+}
--- main.py
+++ main.py
@@ -1,39 +1,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from MRR import MRR
+from importlib import import_module
+import argparse
 
 
-# 結合損
-eta = 1
+def main(config):
+    mrr = MRR(
+        config['eta'],
+        config['n'],
+        config['a'],
+        config['K'],
+        config['L']
+    )
+    y = 20 * np.log10(np.abs(mrr.simulate(config['lambda'])))
+    plt.semilogx(config['lambda'] * 1e9, y)
+    plt.xlabel('Wavelength[nm]')
+    plt.ylabel('Drop Port Power [dB]')
+    plt.title('{} order MRR (same perimeter)'.format(config['L'].size))
+    plt.axis([None, None, None, 5])
+    plt.grid()
+    plt.show()
 
-# 減衰定数
-a = np.array([1, 1])
 
-# 結合効率
-K = np.array([0.65, 0.17, 0.43])
-
-# リング周長
-L = np.array([2.284e-4, 2.284e-4])
-
-# 屈折率
-n = 3.3938
-
-# 波長レンジ
-l = np.arange(1545e-9, 1555e-9, 1e-12)
-
-mrr = MRR(
-    eta,
-    n,
-    a,
-    K,
-    L
-)
-y = 20 * np.log10(np.abs(mrr.simulate(l)))
-
-plt.semilogx(l*1e9, y)
-plt.xlabel('Wavelength[nm]')
-plt.ylabel('Drop Port Power [dB]')
-plt.title('{} order MRR (same perimeter)'.format(L.size))
-plt.axis([None, None, None, 5])
-plt.grid()
-plt.show()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', help='config file path')
+    args = vars(parser.parse_args())
+    try:
+        config = import_module('config.{}'.format(args['config'])).config
+    except:
+        parser.print_help()
+    else:
+        main(config)
