74fa5260b775880b547a5cba92849baf236ea120
Yudai Udagawa <nimius.rd@gmail.com>
2019-06-25 15:12:50 +0900

    Update

--- config/two_same_3.py
+++ config/two_same_3.py
@@ -2,7 +2,7 @@ import numpy as np
 
 config = {
     'eta': 1,  # 結合損
-    'a': np.array([1, 1]),  # 減衰定数
+    'alpha': 0,  # 伝搬損失係数
     'K': np.array([0.65, 0.17, 0.43]),  # 結合効率
     'L': np.array([3e-4, 3e-4]),  # リング周長
     'n': 3.3938,  # 屈折率
