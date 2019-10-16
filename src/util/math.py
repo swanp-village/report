a4d5342bd9d00c533771517d2f4d0a3adcf0a3df
Yudai Udagawa <nimius.rd@gmail.com>
2019-10-16 14:20:13 +0900

    Update

--- src/util/math.py
+++ src/util/math.py
@@ -1,24 +1,25 @@
+from typing import List
 import numpy as np
 from functools import reduce
 
 
-def lcm(xs):
+def lcm(xs: List[float]) -> float:
     return reduce(_lcm, xs)
 
 
-def mod(x, y):
+def mod(x: float, y: float) -> float:
     return x - y * np.floor(x / y)
 
 
-def equals(x, y, eps):
+def equals(x: float, y: float, eps: float) -> bool:
     return np.abs(x - y) < eps
 
 
-def calc_machine_eps(x):
+def calc_machine_eps(x: float) -> float:
     return 10 ** (np.floor(np.log10(x)) - 2)
 
 
-def _gcd(x, y, eps):
+def _gcd(x: float, y: float, eps: float) -> float:
     """
         x < y
     """
@@ -28,7 +29,7 @@ def _gcd(x, y, eps):
         return _gcd(y, mod(x, y), eps)
 
 
-def _lcm(x, y):
+def _lcm(x: float, y: float) -> float:
     if x > y:
         return x * y / _gcd(y, x, calc_machine_eps(y))
     else:
