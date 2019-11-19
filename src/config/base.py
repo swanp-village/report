e6f320c21359b9e2ba57697d7c0f3dcbf3a5f7e7
Yudai Udagawa <nimius.rd@gmail.com>
2019-11-19 15:35:22 +0900

    Update

---
+++ src/config/base.py
@@ -0,0 +1,6 @@
+config = {
+    'eta': 1,  # 結合損
+    'alpha': 0,  # 伝搬損失係数
+    'n': 3.3938,  # 屈折率
+    'center_wavelength': 1550e-9  # 中心波長
+}
--- src/main.py
+++ src/main.py
@@ -5,9 +5,31 @@ import argparse
 from MRR.reward import shape_factor, a
 from MRR.ring import calculate_practical_FSR, find_ring_length
 from MRR.math import _gcd
+import numpy as np
 
 
 def main(config):
+    center_wavelength = config['center_wavelength']
+    n = config['n']
+    _, ring_length_list, FSR_list = find_ring_length(center_wavelength, n)
+    L = np.array([ring_length_list[300], ring_length_list[500], ring_length_list[700]])
+    FSR = calculate_practical_FSR([FSR_list[300], FSR_list[500], FSR_list[700]])
+    K = [0.5, 0.5, 0.5, 0.5]
+    mrr = MRR(
+        config['eta'],
+        n,
+        config['alpha'],
+        K,
+        L
+    )
+    mrr.print_parameters()
+    x = np.arange(center_wavelength - FSR / 2, center_wavelength + FSR / 2, 1e-12)
+    y = mrr.simulate(x)
+    title = '{} order MRR'.format(L.size)
+    plot(x, y, title)
+
+
+def simulate(config):
     mrr = MRR(
         config['eta'],
         config['n'],
@@ -19,31 +41,24 @@ def main(config):
     x = config['lambda']
     y = mrr.simulate(x)
     title = '{} order MRR'.format(config['L'].size)
-    minid, _ = a(x, y)
-    print(x[minid])
-    print(y[minid])
-    N, ring_length_list, FSR_list = find_ring_length(1550e-9, config['n'])
-    # shape_factor(x, y)
-    print(calculate_practical_FSR([FSR_list[300], FSR_list[800]]))
     plot(x, y, title)
-
-    # maxid = argrelmax(y, order=10)[0]
-    # print(y[maxid])
-    # plt.plot(x[maxid] * 1e9, y[maxid], "ro")
-    # sorted_maxid_index = y[maxid].argsort()[::-1]
-    # loss1_id = maxid[sorted_maxid_index[0]]
-    # plt.plot(x[loss1_id] * 1e9, y[loss1_id], 'bo')
-    # loss2_id = maxid[sorted_maxid_index[1]]
-    # plt.plot(x[loss2_id] * 1e9, y[loss2_id], 'go')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', help='config file path')
     args = vars(parser.parse_args())
-    try:
-        config = import_module('config.{}'.format(args['config'])).config
-    except:
-        parser.print_help()
+    if args['config']:
+        try:
+            config = import_module('config.simulate.{}'.format(args['config'])).config
+        except:
+            parser.print_help()
+        else:
+            simulate(config)
     else:
-        main(config)
+        try:
+            config = import_module('config.base').config
+        except:
+            parser.print_help()
+        else:
+            main(config)
