81fff6ce0a78995c36846903066c3855e9b1c38b
Yudai Udagawa <nimius.rd@gmail.com>
2020-01-21 03:31:32 +0900

    Update

--- src/MRR/reward.py
+++ src/MRR/reward.py
@@ -1,110 +1,4 @@
-from typing import List
 import numpy as np
-
-
-def calculate_pass_band_range(x, y, max_loss):
-    pass_band_range = np.array([], dtype=np.int)
-    start = 0
-    end = x.size - 1
-    for i in np.arange(start, end):
-        if y[i] <= max_loss and y[i + 1] > max_loss:  # increase
-            pass_band_range = np.append(pass_band_range, i)
-        elif y[i] >= max_loss and y[i + 1] < max_loss:  # decrease
-            if pass_band_range.size > 0:
-                pass_band_range = np.append(pass_band_range, i)
-            else:
-                pass_band_range = np.append(pass_band_range, [start, i])
-
-    if pass_band_range.size % 2 == 1:
-        pass_band_range = np.append(pass_band_range, end)
-
-    pass_band_range = np.reshape(pass_band_range, [pass_band_range.size // 2, 2])
-
-    return pass_band_range
-
-
-def get_pass_band(x, y, pass_band_range, center_wavelength):
-    pass_band = np.array([], dtype=np.int)
-    cross_talk = np.array([], dtype=np.int)
-    for start, end in pass_band_range:
-        if center_wavelength >= x[start] and center_wavelength <= x[end]:
-            pass_band = np.append(pass_band, [start, end])
-        else:
-            cross_talk = np.append(cross_talk, [start, end])
-
-    pass_band = np.reshape(pass_band, [pass_band.size // 2, 2])
-    cross_talk = np.reshape(cross_talk, [cross_talk.size // 2, 2])
-
-    return pass_band, cross_talk
-
-
-def evaluate_pass_band(x, y, start, end, distance, loss):
-    a = abs(
-        loss * (
-            x[end] - x[start]
-        )
-    )
-    b = abs(
-        sum(
-            loss - y[start:end]
-        ) * distance
-    )
-
-    return b / a
-
-
-def evaluate_stop_band(x, y, start, end, distance, loss, bottom):
-    c = abs(
-        (bottom - loss) * (
-            (x[start] - x[0]) + (x[-1] - x[end])
-        )
-    )
-
-    y1 = np.where(
-        y[0:start] > bottom,
-        loss - y[0:start],
-        loss - bottom
-    )
-    y1 = np.where(
-        y1 > 0,
-        y1,
-        0
-    )
-    y2 = np.where(
-        y[end:-1] > bottom,
-        loss - y[end:-1],
-        loss - bottom
-    )
-    y2 = np.where(
-        y2 > 0,
-        y2,
-        0
-    )
-    d = abs(
-        (
-            sum(y1) + sum(y2)
-        ) * distance
-    )
-
-    return d / c
-
-
-def evaluate_band(x, y, center_wavelength, loss):
-    pass_band_range = calculate_pass_band_range(x, y, loss)
-    pass_band, cross_talk = get_pass_band(x, y, pass_band_range, center_wavelength)
-    # if pass_band.shape[0] == 1 and cross_talk.shape[0] < 1:
-    if pass_band.shape[0] == 1:
-        start = pass_band[0][0]
-        end = pass_band[0][1]
-        distance = x[start] - x[start + 1]
-        bottom = -15
-        print('ok')
-
-        return evaluate_pass_band(x, y, start, end, distance, loss) + evaluate_stop_band(x, y, start, end, distance, loss, bottom)
-    else:
-        print('failed')
-
-        return 0
 
 
 def evaluate_ring_combination(L, FSR_list, practical_FSR):
@@ -123,3 +17,119 @@ def init_action(number_of_rings):
         action.extend(a_i.tolist())
 
     return action
+
+
+class Reward:
+    def __init__(
+        self,
+        x,
+        y,
+        center_wavelength,
+        number_of_rings,
+        loss,
+        bottom
+    ):
+        self.x = x
+        self.y = y
+        self.distance = x[1] - x[0]
+        self.center_wavelength = center_wavelength
+        self.number_of_rings = number_of_rings
+        self.loss = loss
+        self.bottom = bottom
+
+    def calculate_pass_band_range(self):
+        pass_band_range = np.array([], dtype=np.int)
+        start = 0
+        end = self.x.size - 1
+        for i in np.arange(start, end):
+            if self.y[i] <= self.loss and self.y[i + 1] > self.loss:  # increase
+                pass_band_range = np.append(pass_band_range, i)
+            elif self.y[i] >= self.loss and self.y[i + 1] < self.loss:  # decrease
+                if pass_band_range.size > 0:
+                    pass_band_range = np.append(pass_band_range, i)
+                else:
+                    pass_band_range = np.append(pass_band_range, [start, i])
+
+        if pass_band_range.size % 2 == 1:
+            pass_band_range = np.append(pass_band_range, end)
+
+        pass_band_range = np.reshape(pass_band_range, [pass_band_range.size // 2, 2])
+
+        return pass_band_range
+
+    def get_pass_band(self):
+        pass_band = np.array([], dtype=np.int)
+        cross_talk = np.array([], dtype=np.int)
+        for start, end in self.calculate_pass_band_range():
+            if self.center_wavelength >= self.x[start] and self.center_wavelength <= self.x[end]:
+                pass_band = np.append(pass_band, [start, end])
+            else:
+                cross_talk = np.append(cross_talk, [start, end])
+
+        pass_band = np.reshape(pass_band, [pass_band.size // 2, 2])
+        cross_talk = np.reshape(cross_talk, [cross_talk.size // 2, 2])
+
+        return pass_band, cross_talk
+
+    def evaluate_band(self):
+        pass_band, cross_talk = self.get_pass_band()
+        # if pass_band.shape[0] == 1 and cross_talk.shape[0] < 1:
+        if pass_band.shape[0] == 1:
+            start = pass_band[0][0]
+            end = pass_band[0][1]
+            print('ok')
+
+            return self.evaluate_pass_band(start, end) + self.evaluate_stop_band(start, end)
+        else:
+            print('failed')
+
+            return 0
+
+    def evaluate_pass_band(self, start, end):
+        a = abs(
+            self.loss * (
+                self.x[end] - self.x[start]
+            )
+        )
+        b = abs(
+            sum(
+                self.loss - self.y[start:end]
+            ) * self.distance
+        )
+
+        return b / a
+
+    def evaluate_stop_band(self, start, end):
+        c = abs(
+            (self.bottom - self.loss) * (
+                (self.x[start] - self.x[0]) + (self.x[-1] - self.x[end])
+            )
+        )
+
+        y1 = np.where(
+            self.y[0:start] > self.bottom,
+            self.loss - self.y[0:start],
+            self.loss - self.bottom
+        )
+        y1 = np.where(
+            y1 > 0,
+            y1,
+            0
+        )
+        y2 = np.where(
+            self.y[end:-1] > self.bottom,
+            self.loss - self.y[end:-1],
+            self.loss - self.bottom
+        )
+        y2 = np.where(
+            y2 > 0,
+            y2,
+            0
+        )
+        d = abs(
+            (
+                sum(y1) + sum(y2)
+            ) * self.distance
+        )
+
+        return d / c
--- src/main.py
+++ src/main.py
@@ -3,36 +3,12 @@ from MRR.gragh import plot
 from importlib import import_module
 import argparse
 import numpy as np
-from MRR.reward import evaluate_band, evaluate_ring_combination, init_action
+from MRR.reward import Reward, evaluate_ring_combination, init_action
 from MRR.ring import calculate_x, calculate_practical_FSR, find_ring_length, init_K, init_N, calculate_ring_length, calculate_FSR
 
 
 def main(config):
-    center_wavelength = config['center_wavelength']
-    number_of_rings = config['number_of_rings']
-    max_loss_in_pass_band = config['max_loss_in_pass_band']
-    max_N = config['max_N']
-    n = config['n']
-    ring_length_list, FSR_list = find_ring_length(center_wavelength, n, max_N)
-    index = [39, 831]
-    L = ring_length_list[index]
-    FSR = calculate_practical_FSR(FSR_list[index])
-    K = init_K(number_of_rings)
-    mrr = MRR(
-        config['eta'],
-        n,
-        config['alpha'],
-        K,
-        L
-    )
-    x = calculate_x(center_wavelength, FSR)
-    y = mrr.simulate(x)
-    result = evaluate_pass_band(x, y, center_wavelength, max_loss_in_pass_band)
-    # if result:
-    mrr.print_parameters()
-    title = '{} order MRR'.format(L.size)
-    plot(x, y, title)
-
+    pass
 
 def train(config):
     number_of_episodes = config['number_of_episodes']
@@ -70,7 +46,16 @@ def train(config):
                     )
                     x = calculate_x(center_wavelength, FSR)
                     y = mrr.simulate(x)
-                    result = evaluate_band(x, y, center_wavelength, max_loss_in_pass_band)
+
+                    reward = Reward(
+                        x,
+                        y,
+                        center_wavelength,
+                        number_of_rings,
+                        max_loss_in_pass_band,
+                        -15
+                    )
+                    result = reward.evaluate_band()
                     if result > 0:
                         print(result)
                         # mrr.print_parameters()
@@ -92,7 +77,15 @@ def train(config):
         )
         x = calculate_x(center_wavelength, FSR)
         y = mrr.simulate(x)
-        result = evaluate_band(x, y, center_wavelength, max_loss_in_pass_band)
+        reward = Reward(
+            x,
+            y,
+            center_wavelength,
+            number_of_rings,
+            max_loss_in_pass_band,
+            -15
+        )
+        result = reward.evaluate_band()
         if result > 0:
             print(result)
             mrr.print_parameters()
@@ -111,7 +104,6 @@ def simulate(config):
     mrr.print_parameters()
     x = config['lambda']
     y = mrr.simulate(x)
-    result = evaluate_pass_band(x, y, 1550e-9, -5)
     title = '{} order MRR'.format(config['L'].size)
     plot(x, y, title)
 
