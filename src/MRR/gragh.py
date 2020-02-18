05a03ad55104f2c89b207c43cdd4d2d508fc22df
Yudai Udagawa <nimius.rd@gmail.com>
2020-02-18 14:20:38 +0900

    Update

--- src/MRR/gragh.py
+++ src/MRR/gragh.py
@@ -1,5 +1,10 @@
 import matplotlib.pyplot as plt
 from matplotlib import rc
+from matplotlib.ticker import FuncFormatter
+
+@FuncFormatter
+def formatter(x, pos):
+    return "%d" % x
 
 
 def plot(x, y, number_of_rings, img_path='img/out.pdf'):
@@ -14,8 +19,14 @@ def plot(x, y, number_of_rings, img_path='img/out.pdf'):
         title = '{}th order MRR'.format(number_of_rings)
     fig, ax = plt.subplots()
     ax.semilogx(x * 1e9, y)
-    ax.set(xlabel='Wavelength[nm]', ylabel='Drop Port Power [dB]', title=title)
+    ax.set(
+        xlabel='Wavelength[nm]',
+        ylabel='Drop Port Power [dB]',
+        title=title
+    )
     ax.axis([None, None, None, 5])
-    ax.grid()
+    ax.xaxis.set_major_formatter(formatter)
+    ax.xaxis.set_minor_formatter(formatter)
+    ax.grid(which='both')
     fig.savefig(img_path)
     plt.show()
