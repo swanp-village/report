167f02c2eea388b7bd5c318a3f5f7900fbceb17a
Yudai Udagawa <nimius.rd@gmail.com>
2021-01-09 19:03:26 +0900

    Update

--- src/config/base.py
+++ src/config/base.py
@@ -1,16 +1,17 @@
 config = {
-    'eta': 1,  # 結合損
-    'alpha': 52.96,  # 伝搬損失係数
-    'n_eff': 3.3938,  # 等価屈折率
-    'n_g': 4.2,  # 実行屈折率
-    'number_of_rings': 8,  # リング次数
-    'center_wavelength': 1550e-9,  # 中心波長
-    'FSR': 20e-9,
-    'min_ring_length': 50e-6,
+    'eta': 0.996,
+    'alpha': 52.96,
+    'n_eff': 3.3938,
+    'n_g': 4.2,
 
-    'max_loss_in_pass_band': -30,
-    'required_loss_in_stop_band': -60,
+    'number_of_rings': 4,
+    'center_wavelength': 1550e-9,
+    'FSR': 20e-9,
     'length_of_3db_band': 1e-9,
 
-    'number_of_episodes_in_L': 30
+    'max_loss_in_pass_band': -40,
+    'required_loss_in_stop_band': -60,
+    'min_ring_length': 50e-6,
+
+    'number_of_episodes_in_L': 2
 }
