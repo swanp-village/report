d587e58d3b25bf58a6007c5b35d8f71df6c74b67
Yudai Udagawa <nimius.rd@gmail.com>
2021-01-11 02:34:13 +0900

    Update

--- src/MRR/Evaluator/Model/data/__init__.py
+++ src/MRR/Evaluator/Model/data/__init__.py
@@ -1,14 +1,44 @@
-from .stopband import data as stopband
-from .ripple import data as ripple
+from random import sample
+from math import floor
 from .crosstalk import data as crosstalk
-from .three_db_band import data as three_db_band
 from .insertion_loss import data as insertion_loss
+from .ripple import data as ripple
+from .stopband import data as stopband
+from .three_db_band import data as three_db_band
 
 
-data = [
+all_data = [
     *crosstalk,
     *insertion_loss,
     *ripple,
     *stopband,
     *three_db_band,
 ]
+
+def split(data, test_size):
+    length = len(data)
+    test_length = floor(length * test_size)
+    shuffled_data = sample(data, length)
+    test = shuffled_data[:test_length]
+    train = shuffled_data[test_length:]
+
+    return train, test
+
+
+def get_splited_data(test_size=0.25):
+    data = [
+        crosstalk,
+        insertion_loss,
+        ripple,
+        stopband,
+        three_db_band,
+    ]
+    train = []
+    test = []
+
+    for d in data:
+        d_train, d_test = split(d, test_size)
+        train = [*train, *d_train]
+        test = [*test, *d_test]
+
+    return train, test
--- src/MRR/Evaluator/Model/train.py
+++ src/MRR/Evaluator/Model/train.py
@@ -1,21 +1,21 @@
-from .data import data
+from .data import all_data, get_splited_data
 from MRR.Evaluator.evaluator import build_Evaluator
 import numpy as np
 from .config import config
 from itertools import product
 from scipy.cluster.vq import kmeans, whiten
 from sklearn.svm import SVC
-from sklearn.model_selection import LeaveOneOut, cross_val_score
+from sklearn.model_selection import cross_validate
 from multiprocessing import Pool
 
 weight_of_binary_evaluation = np.array([0.5])
-weight_list = product(
+weight_list = list(product(
     *[
-        np.arange(1, 6, 0.5),
-        np.arange(1, 6, 0.5),
-        np.arange(1, 6, 0.5),
-        np.arange(1, 6, 0.5),
-        np.arange(1, 6, 0.5),
+        np.arange(1, 6, 0.1),
+        np.arange(1, 6, 0.1),
+        np.arange(1, 6, 0.1),
+        np.arange(1, 6, 0.1),
+        np.arange(1, 6, 0.1),
         np.array([1]),
         weight_of_binary_evaluation,
         weight_of_binary_evaluation,
@@ -24,50 +24,71 @@ weight_list = product(
         weight_of_binary_evaluation,
         weight_of_binary_evaluation
     ]
-)
-rank_list = np.array([
+))
+train, test = get_splited_data(0.2)
+train_rank_list = np.array([
     d.rank
-    for d in data
+    for d in train
 ])
+test_rank_list = np.array([
+    d.rank
+    for d in test
+])
+def train_data(weight):
+    Evaluator = build_Evaluator(config, weight)
+    train_evaluate_result = np.array([
+        Evaluator(
+            d.x,
+            d.y
+        ).evaluate_band()
+        for d in train
+    ])
+    test_evaluate_result = np.array([
+        Evaluator(
+            d.x,
+            d.y
+        ).evaluate_band()
+        for d in test
+    ])
+    train_evaluate_result = np.array(train_evaluate_result).reshape(-1, 1)
+    test_evaluate_result = np.array(test_evaluate_result).reshape(-1, 1)
+    X_train = train_evaluate_result
+    y_train = train_rank_list
+    X_test = test_evaluate_result
+    y_test = test_rank_list
+    clf = SVC()
+    clf.fit(X_train, y_train)
+    score = clf.score(X_test, y_test)
+    print('==========================')
+    print(weight)
+    print(score)
+
+    return score
 
 
 def train_evaluator():
     max_score = 0
     optimized_weight = []
-    for weight in weight_list:
-        print('==========================')
-        print(weight)
 
-        Evaluator = build_Evaluator(config, weight)
-        evaluate_result = np.array([
-            Evaluator(
-                data_i.x,
-                data_i.y
-            ).evaluate_band()
-            for data_i in data
-        ])
-        print(evaluate_result)
-        evaluate_result = np.array(evaluate_result).reshape(-1, 1)
-        X = evaluate_result
-        y = rank_list
-        clf = SVC()
-        scores = cross_val_score(clf, X, y, cv=4, scoring='f1_micro', n_jobs=-1)
-        average_score = scores.mean()
-        print(scores.mean(), scores)
+    with Pool() as p:
+        scores = p.map(train_data, weight_list)
 
-        if average_score > max_score:
-            max_score = average_score
-            optimized_weight = weight
+    scores = np.array(scores)
+    weights = np.array(weight_list)
+    max_score = np.max(scores)
+    optimized_weights = weights[scores == max_score]
+    print(max_score)
+    print(optimized_weights)
 
-    print(max_score, optimized_weight)
     with open('result', 'w') as f:
-        f.write('{} {}'.format(max_score, optimized_weight))
+        f.write('{}\n'.format(max_score))
+        f.write('{}'.format(optimized_weights))
 
 
 
 def show_data(skip_plot):
     Evaluator = build_Evaluator(config)
-    for data_i in data:
+    for data_i in all_data:
         evaluator = Evaluator(
             data_i.x,
             data_i.y
