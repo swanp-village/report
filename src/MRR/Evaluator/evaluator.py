b2fd23b7de29feae00d48d151a6d1ee373dd2f4c
Yudai Udagawa <nimius.rd@gmail.com>
2021-01-14 21:11:20 +0900

    Update

--- src/MRR/Evaluator/evaluator.py
+++ src/MRR/Evaluator/evaluator.py
@@ -230,18 +230,18 @@ class build_Evaluator_Factory:
         return Evaluator(L, K, self.weight, self.config)
 
 def build_Evaluator(config, weight=[
+            1.0,
+            1.0,
+            3.0,
+            5.0,
             1.5,
-            5.5,
-            4.0,
-            1.0,
-            2.5,
             1.0,
             0.5,
             0.5,
             0.5,
             0.5,
             0.5,
-            0.5
+            0.5,
         ]):
     """Partial-apply config to Evaluator
 
