1fe1d8aca2f776eaecf51d49f308fc72e8e8c693
Yudai Udagawa <nimius.rd@gmail.com>
2021-01-19 04:33:20 +0900

    Update

--- src/config/simulate/6_DE1.py
+++ src/config/simulate/6_DE1.py
@@ -23,5 +23,5 @@ config = {
     'n_eff': 3.3938,  # 実行屈折率
     'n_g': 4.2,  # 群屈折率
     'center_wavelength': 1550e-9,
-    'lambda': np.arange(1525e-9, 1555e-9, 1e-12)
+    # 'lambda': np.arange(1525e-9, 1555e-9, 1e-12)
 }
