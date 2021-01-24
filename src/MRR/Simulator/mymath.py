ef9acae7418cee61ea15818139c61d69aa689d4f
Kaito Udagawa <umireon@gmail.com>
2021-01-24 18:04:37 +0900

    Backward compatibility

--- src/MRR/Simulator/mymath.py
+++ src/MRR/Simulator/mymath.py
@@ -24,11 +24,11 @@ def _gcd(x: float, y: float) -> float:
         x > y
     """
     n = 0
-    while y != 0 and not is_zero(x, y) and n < 10:
+    while y != 0 and not is_zero(x, y) and n <= 10:
         x, y = y, mod(x, y)
         n += 1
 
-    if is_zero(x, y):
+    if is_zero(x, y) or n > 10
         return y
     else:
         return x
