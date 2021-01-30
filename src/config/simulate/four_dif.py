c1bfaca3e41addfc5cd572173cab5347b77d191a
Yudai Udagawa <nimius.rd@gmail.com>
2021-01-31 03:25:06 +0900

    Update

--- src/config/simulate/four_dif.py
+++ src/config/simulate/four_dif.py
@@ -7,5 +7,6 @@ config = {
     'L': np.array([4.56715187e-05, 6.85072780e-05, 2.74029112e-05, 9.13430373e-05]),  # リング周長
     'n_eff': 3.3938,  # 実行屈折率
     'n_g': 4.2,  # 群屈折率
-    'center_wavelength': 1550e-9
+    'center_wavelength': 1550e-9,
+    # 'lambda': np.arange(1490e-9, 1560e-9, 1e-12)
 }
