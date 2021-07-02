818138574274d86e0d05b6c2249a293aa86e2c85
Yudai Udagawa <nimius.rd@gmail.com>
2021-07-02 14:57:21 +0900

    Update

--- startup-script.sh
+++ startup-script.sh
@@ -15,10 +15,6 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 ulimit -n 8096
+python main.py --skip-plot
 
-# tmux new -s train
-# tmux a -t train
-# tmux kill-session -t train
-
-# python main.py --skip-plot
 # python main.py --train-evaluator
