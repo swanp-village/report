f205e9d7f19afa6224976ada45e9d08da3cf3d54
Yudai Udagawa <nimius.rd@gmail.com>
2021-08-03 21:11:02 +0900

    Update

--- startup-script.sh
+++ startup-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install or update needed software
 sudo apt-get update
-sudo apt-get install -yq git python3.8 python3-venv
+sudo apt-get install -yq git python3.9 python3-venv
 
 # Fetch source code
 git clone https://github.com/nimiusrd/MRR.git
