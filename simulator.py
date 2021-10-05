0f118f9d699e6cbe0c524ce7398db82a4fd3b344
Yudai Udagawa <nimius.rd@gmail.com>
2021-10-06 06:43:19 +0900

    Update

--- simulator.py
+++ simulator.py
@@ -1,6 +1,8 @@
 import argparse
 import csv
+import os.path
 import subprocess
+from glob import glob
 from importlib import import_module
 from pathlib import Path
 
@@ -31,12 +33,14 @@ def plot_with_pgfplots(basedir: Path, results: list[SimulatorResult], is_focus: 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("NAME", help="from config.simulate import NAME", nargs="+")
+    parser.add_argument("NAME", help="from config.simulate import NAME", nargs="*")
+    parser.add_argument("-l", "--list", action="store_true")
     parser.add_argument("--skip-plot", action="store_true")
     parser.add_argument("--format", action="store_true")
     parser.add_argument("-f", "--focus", action="store_true")
     parser.add_argument("-s", "--simulate-one-cycle", action="store_true")
     args = vars(parser.parse_args())
+    ls = args["list"]
     skip_plot = args["skip_plot"]
     is_focus = args["focus"]
     format = args["format"]
@@ -44,21 +48,24 @@ if __name__ == "__main__":
 
     results: list[SimulatorResult] = []
     simulator = Simulator(is_focus)
-    for name in args["config"]:
-        try:
-            imported_module = import_module(f"config.simulate.{name}")
-            imported_config = getattr(imported_module, "config")
-            simulation_config = SimulationConfig(**imported_config)
-            simulation_config.name = name
-            simulation_config.format = format
-            simulation_config.simulate_one_cycle = simulate_one_cycle
-            result = simulator.simulate(simulation_config)
-            results.append(result)
-            print("E:", result.evaluation_result)
-        except ModuleNotFoundError as e:
-            print(e)
+    if ls:
+        print("\t".join([os.path.splitext(os.path.basename(p))[0] for p in sorted(glob("config/simulate/*.py"))]))
+    else:
+        for name in args["config"]:
+            try:
+                imported_module = import_module(f"config.simulate.{name}")
+                imported_config = getattr(imported_module, "config")
+                simulation_config = SimulationConfig(**imported_config)
+                simulation_config.name = name
+                simulation_config.format = format
+                simulation_config.simulate_one_cycle = simulate_one_cycle
+                result = simulator.simulate(simulation_config)
+                results.append(result)
+                print("E:", result.evaluation_result)
+            except ModuleNotFoundError as e:
+                print(e)
 
-    plot_with_pgfplots(simulator.logger.target, results, simulator.graph.is_focus)
+        plot_with_pgfplots(simulator.logger.target, results, simulator.graph.is_focus)
 
-    if not skip_plot:
-        simulator.show()
+        if not skip_plot:
+            simulator.show()
