f723c557659d63c63d6261e052ab18d290fdda4c
Yudai Udagawa <nimius.rd@gmail.com>
2021-10-14 16:41:56 +0900

    Update

--- snippet/plt5.py
+++ snippet/plt5.py
@@ -8,36 +8,62 @@ fig, ax = plt.subplots(figsize=(8, 6))
 rc("text", usetex=True)
 rc("font", size=16)
 
-with open("snippet/data/analyzer_result_10th.txt") as f:
+with open("snippet/data/N10L1K10/analyzer_result.txt") as f:
     reader = csv.reader(f)
-    ring10 = [float(row[0]) for row in reader]
-    ring10_hist, bin_edges = np.histogram(ring10, range=(0, 15), bins=15 * 4)
+    ring10L1K10 = [float(row[0]) for row in reader]
+    ring10L1K10_hist, bin_edges = np.histogram(ring10L1K10, range=(0, 15), bins=15 * 4)
     bin_centers = (bin_edges[1:] + bin_edges[:-1]) / 2
 
-with open("snippet/data/analyzer_result_8th.txt") as f:
+with open("snippet/data/N8L1K10/analyzer_result.txt") as f:
     reader = csv.reader(f)
-    ring8 = [float(row[0]) for row in reader]
-    ring8_hist, _ = np.histogram(ring8, range=(0, 15), bins=15 * 4)
+    ring8L1K10 = [float(row[0]) for row in reader]
+    ring8L1K10_hist, _ = np.histogram(ring8L1K10, range=(0, 15), bins=15 * 4)
 
 
-with open("snippet/data/analyzer_result_6th.txt") as f:
+with open("snippet/data/N6L1K10/analyzer_result.txt") as f:
     reader = csv.reader(f)
-    ring6 = [float(row[0]) for row in reader]
-    ring6_hist, _ = np.histogram(ring6, range=(0, 15), bins=15 * 4)
+    ring6L1K10 = [float(row[0]) for row in reader]
+    ring6L1K10_hist, _ = np.histogram(ring6L1K10, range=(0, 15), bins=15 * 4)
 
-with open("snippet/data/analyzer_result_4th.txt") as f:
+with open("snippet/data/N4L1K10/analyzer_result.txt") as f:
     reader = csv.reader(f)
-    ring4 = [float(row[0]) for row in reader]
-    ring4_hist, _ = np.histogram(ring4, range=(0, 15), bins=15 * 4)
+    ring4L1K10 = [float(row[0]) for row in reader]
+    ring4L1K10_hist, _ = np.histogram(ring4L1K10, range=(0, 15), bins=15 * 4)
+
+with open("snippet/data/N10L10K10/analyzer_result.txt") as f:
+    reader = csv.reader(f)
+    ring10L10K10 = [float(row[0]) for row in reader]
+    ring10L10K10_hist, bin_edges = np.histogram(ring10L10K10, range=(0, 15), bins=15 * 4)
+    bin_centers = (bin_edges[1:] + bin_edges[:-1]) / 2
+
+with open("snippet/data/N8L10K10/analyzer_result.txt") as f:
+    reader = csv.reader(f)
+    ring8L10K10 = [float(row[0]) for row in reader]
+    ring8L10K10_hist, _ = np.histogram(ring8L10K10, range=(0, 15), bins=15 * 4)
+
+
+with open("snippet/data/N6L10K10/analyzer_result.txt") as f:
+    reader = csv.reader(f)
+    ring6L10K10 = [float(row[0]) for row in reader]
+    ring6L10K10_hist, _ = np.histogram(ring6L10K10, range=(0, 15), bins=15 * 4)
+
+with open("snippet/data/N4L10K10/analyzer_result.txt") as f:
+    reader = csv.reader(f)
+    ring4L10K10 = [float(row[0]) for row in reader]
+    ring4L10K10_hist, _ = np.histogram(ring4L10K10, range=(0, 15), bins=15 * 4)
 
 # plt.hist(ring10, range=(0, 15), bins=15 * 4, label="10th", histtype="step")
 # plt.hist(ring8, range=(0, 15), bins=15 * 4, label="8th", histtype="step")
 # plt.hist(ring6, range=(0, 15), bins=15 * 4, label="6th", histtype="step")
 # plt.hist(ring4, range=(0, 15), bins=15 * 4, label="4th", histtype="step")
-plt.plot(bin_centers, ring4_hist, label="4th")
-plt.plot(bin_centers, ring6_hist, label="6th")
-plt.plot(bin_centers, ring8_hist, label="8th")
-plt.plot(bin_centers, ring10_hist, label="10th")
+plt.plot(bin_centers, ring4L1K10_hist, label="4th L:1%")
+# plt.plot(bin_centers, ring6L1K10_hist, label="6th")
+# plt.plot(bin_centers, ring8L1K10_hist, label="8th")
+plt.plot(bin_centers, ring10L1K10_hist, label="10th L:1%")
+plt.plot(bin_centers, ring4L10K10_hist, label="4th L:10%")
+# plt.plot(bin_centers, ring6L10K10_hist, label="6th")
+# plt.plot(bin_centers, ring8L10K10_hist, label="8th")
+plt.plot(bin_centers, ring10L10K10_hist, label="10th L:10%")
 plt.ylabel("frequency", size=20)
 plt.xlabel("evaluation function value", size=20)
 plt.legend(loc="upper right")
