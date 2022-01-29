d40b83b4485dc89c433e6ac30e3354215167ef47
Yudai Udagawa <nimius.rd@gmail.com>
2022-01-30 02:22:39 +0900

    Update

--- config/base.py
+++ config/base.py
@@ -5,7 +5,7 @@ config = OptimizationConfig(
     alpha=52.96,
     n_eff=2.2,
     n_g=4.4,
-    number_of_rings=12,
+    number_of_rings=8,
     center_wavelength=1550e-9,
     FSR=20e-9,
     length_of_3db_band=1e-9,
@@ -17,5 +17,5 @@ config = OptimizationConfig(
     weight=[1.0, 3.5, 1.0, 5.0, 3.5, 1.0, 1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
     min_ring_length=50e-6,
     number_of_generations=300,
-    strategy=[0, 0, 0, 1.0],
+    strategy=[0.03, 0.07, 0.2, 0.7],
 )
