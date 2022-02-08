acf0f6a7b9872f442a687c884b176cab87165483
Yudai Udagawa <nimius.rd@gmail.com>
2022-02-09 06:45:32 +0900

    Update

--- MRR/graph.py
+++ MRR/graph.py
@@ -25,13 +25,14 @@ class Graph:
         label: Optional[str] = None,
     ) -> None:
         self.ax.semilogx(x * 1e9, y, label=label)
+        # self.ax.semilogx(x * 1e9, y, label=label, lw=1)
 
     def show(
         self,
         img_path: Path = Path("img/out.pdf"),
     ) -> None:
-        self.ax.set_xlabel("Wavelength (nm)", fontsize=24)
-        self.ax.set_ylabel("Drop Port Power (dB)", fontsize=24)
+        self.ax.set_xlabel(r"Wavelength \(\lambda\) (nm)", fontsize=24)
+        self.ax.set_ylabel("Transmittance (dB)", fontsize=24)
         self.ax.axis([None, None, None, 5])
 
         if self.is_focus:
@@ -46,6 +47,8 @@ class Graph:
             self.ax.xaxis.set_minor_formatter(FormatStrFormatter("%d"))
             self.ax.xaxis.set_major_locator(AutoLocator())
             self.ax.set_ylim([-60, 0])
-        plt.legend(loc="lower right")
+        plt.legend(loc="upper center", fontsize=12, frameon=False)
+        #
+        # plt.legend(loc="lower right", ncol=2)
         self.fig.savefig(img_path)
         plt.show()
