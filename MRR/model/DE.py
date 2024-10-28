9aa0108f78a9be54c9ed63c28bf5a6004e413c21
swanp-village <92138575+swanp-village@users.noreply.github.com>
2024-10-29 03:56:16 +0900

    Update DE.py

--- MRR/model/DE.py
+++ MRR/model/DE.py
@@ -74,16 +74,16 @@ def optimize_K(
 ) -> tuple[npt.NDArray[np.float_], float]:
     bounds = [(1e-12, eta) for _ in range(number_of_rings + 1)]
 
-    ##result = differential_evolution (
-        ##optimize_K_func,
-        ##bounds,
-        ##args=(params,),
-        ##strategy="currenttobest1bin",
-        ##workers=-1,
-        ##updating="deferred",
-        ##popsize=15,
-        ##maxiter=500,
-        ##seed=rng,
+    result = differential_evolution (
+        optimize_K_func,
+        bounds,
+        args=(params,),
+        strategy="currenttobest1bin",
+        workers=-1,
+        updating="deferred",
+        popsize=15,
+        maxiter=500,
+        seed=rng,
     ##)
     result=CMA(
         self,
