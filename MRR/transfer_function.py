17482ea6b81ae663e47e6c27135421679d8a3a40
swanp-village <92138575+swanp-village@users.noreply.github.com>
2024-10-31 18:00:19 +0900

    Update transfer_function.py

--- MRR/transfer_function.py
+++ MRR/transfer_function.py
@@ -12,9 +12,9 @@ def simulate_transfer_function(
     n_g: float,
     center_wavelength: float,
 ) -> npt.NDArray[np.float_]:
-    print(K)
-    K=K.T
-    print(K)
+    #print(K)
+    #K=K.T
+    #print(K)
     y: npt.NDArray[np.float_] = 20 * np.log10(np.abs(_D(L, K, alpha, wavelength, eta, n_eff, n_g, center_wavelength)))
     return y.reshape(y.size)
 
