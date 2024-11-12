8fa5e88499777a77a6c961819661b7f55efa1d44
swanp-village <92138575+swanp-village@users.noreply.github.com>
2024-11-13 00:01:15 +0900

    Update DE.py

--- MRR/model/DE.py
+++ MRR/model/DE.py
@@ -109,13 +109,13 @@ def optimize_K(
     #print("Test value:", test_value)
     for _ in range(3):  # 500世代実行する場合
       solutions = optimizer.ask()  # 解を生成
-      print(solutons)
+      print(solutions)
       print(np.shape(solutions))
       fitness = np.array([float(optimize_K_func(K,params)) for K in solutions]) # 各解の評価
       print(fitness)
       print("Fitness values shape:", np.shape(fitness))
           
-      solutions_with_fitness =[(solution, fitness_value) for solution, fitness_value in zip(solutions, fitness)]
+      solutions_with_fitness =[(solutions, fitness_value) for solution, fitness_value in zip(solutions, fitness)]
       print("solution_with_fitness shape:",np.shape(solutions_with_fitness))
       
       optimizer.tell(solutions_with_fitness)  # 評価結果を最適化アルゴリズムに渡す
