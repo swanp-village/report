97b0660ae45b5383afa4af266c06280b539b3f87
swanp-village <92138575+swanp-village@users.noreply.github.com>
2024-11-12 15:40:41 +0900

    Update DE.py

--- MRR/model/DE.py
+++ MRR/model/DE.py
@@ -112,7 +112,7 @@ def optimize_K(
       #print(solutions)
       #print("Solutions type:", type(solutions))
       #print("Solutions shape:", solutions.shape)
-      fitness = [float(optimize_K_func(K,params)) for K in solutions] # 各解の評価
+      fitness = np.array([optimize_K_func(K,params) for K in solutions]) # 各解の評価
       #print("Fitness values shape:", np.shape(fitness))
       #print("Fitness values:", fitness.shape)
       optimizer.tell(fitness)  # 評価結果を最適化アルゴリズムに渡す
