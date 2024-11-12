5fd5944e08dbc1bfa027cf7d96fa3ff03011a5b0
swanp-village <92138575+swanp-village@users.noreply.github.com>
2024-11-12 18:41:22 +0900

    Update DE.py

--- MRR/model/DE.py
+++ MRR/model/DE.py
@@ -94,7 +94,23 @@ def optimize_K(
         #maxiter=500,
         #seed=rng,
     #)
-    
+     def quadratic(x1, x2):
+               return (x1 - 3) ** 2 + (10 * (x2 + 2)) ** 2
+
+           optimizer = CMA(mean=np.zeros(2), sigma=1.3)
+
+    for generation in range(50):
+               solutions = []
+               for _ in range(optimizer.population_size):
+                   # Ask a parameter
+                   x = optimizer.ask()
+                   value = quadratic(x[0], x[1])
+                   solutions.append((x, value))
+                   print(f"#{generation} {value} (x1={x[0]}, x2 = {x[1]})")
+
+               # Tell evaluation values.
+               optimizer.tell(solutions)
+
 
     sigma=0.2
     optimizer=CMA(
