a863ddbb736eaf0725b4f41ccd4e64339a43dad7
swanp-village <92138575+swanp-village@users.noreply.github.com>
2024-11-12 15:34:02 +0900

    Update DE.py

--- MRR/model/DE.py
+++ MRR/model/DE.py
@@ -82,7 +82,7 @@ def optimize_K(
     #for i in range(number_of_rings+1)
     initial=rng.uniform(1e-12, eta,size=(1,len(bounds))) 
     initial_T=initial.T
-    popsize=15
+    popsize=number_of_rings+1
     #result = differential_evolution(
      #   optimize_K_func,
       #  bounds,
