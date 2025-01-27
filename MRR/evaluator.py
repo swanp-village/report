cbbd66608ee8208ef656f8a58209334860ab4d99
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-01-27 20:54:18 +0900

    Update evaluator.py

--- MRR/evaluator.py
+++ MRR/evaluator.py
@@ -145,11 +145,11 @@ def _evaluate_insertion_loss(
 ) -> tuple[np.float_, bool]:
     insertion_loss = y[x == center_wavelength]
     if insertion_loss[0] < H_i:
-        #E = H_i / insertion_loss[0]
-        return (np.float_(0), False)
-    #else:
-        #E = 1 - insertion_loss[0] / H_i
-    E = 1 - insertion_loss[0] / H_i
+        E = H_i / insertion_loss[0]
+        #return (np.float_(0), False)
+    else:
+        E = 1 - insertion_loss[0] / H_i
+    #E = 1 - insertion_loss[0] / H_i
     return (E, True)
 
 
