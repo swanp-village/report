37061fc1b27399cabb3391cff87a3b5d8f5e2ad3
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-01-28 14:39:40 +0900

    Update evaluator.py

--- MRR/evaluator.py
+++ MRR/evaluator.py
@@ -319,7 +319,7 @@ def _evaluate_ripple(
         E = 1 - dif / r_max
 
     return (E, True)
-
+"""
 def _evaluate_cross_talk(
     y: npt.NDArray[np.float_], max_crosstalk: float, pass_band_start: int, pass_band_end: int
 ) -> tuple[np.float_, bool]:
@@ -334,8 +334,8 @@ def _evaluate_cross_talk(
     if a or b:
         return (np.float_(0), False)
     return (np.float_(0), True)
-
 """
+
 def _evaluate_cross_talk(
     y: npt.NDArray[np.float_], max_crosstalk: float, pass_band_start: int, pass_band_end: int
 ) -> tuple[np.float_, bool]:
@@ -349,7 +349,7 @@ def _evaluate_cross_talk(
     excess_end = np.maximum(end_peak - max_crosstalk, 0)
     penalty = np.sum(excess_start) + np.sum(excess_end)
     return (1 / (1 + penalty), True)
-"""
+
 
 def _evaluate_shape_factor(
     x: npt.NDArray[np.float_], y: npt.NDArray[np.float_], start: int, end: int
