ef7143a25e0210a5728cdb1e655373ecf34ced6e
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-02-10 14:39:39 +0900

    Update graph.py

--- MRR/graph.py
+++ MRR/graph.py
@@ -22,7 +22,7 @@ class Graph:
         self,
         x: npt.NDArray[np.float_],
         y: npt.NDArray[np.float_],
-        label: self.ax.semilogx(x * 1e9, y, label=label)
+        label: self.ax.semilogx(x * 1e9, y, label=label),
     )
     def show_legend(self):
         """凡例を表示"""
