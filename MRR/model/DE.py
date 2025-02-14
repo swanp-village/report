feb83a3c96ba50306340592c405807f05ae446a3
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-02-14 11:09:47 +0900

    Update DE.py

--- MRR/model/DE.py
+++ MRR/model/DE.py
@@ -118,11 +118,10 @@ def optimize_K(
     bounds = [(1e-12, eta) for _ in range(number_of_rings + 1)]
     bounds_array=np.array(bounds) 
     #initial=np.random.uniform(1e-12, eta, size=(number_of_rings+1,))
-    #sampler = LatinHypercube(d=number_of_rings+1)  # 次元数を1に設定
-    #samples = sampler.random(n=1)  # 1サンプルだけ生成（shape: (1, number_of_rings + 1)）
-    #initial = np.array([0.31696919,0.03971662,0.03015172,0.03758653,0.06327071,0.11668272,0.6309192])
-    initial = np.array([0.47861465815127613, 0.059825303636854066, 0.02833346559136013, 0.03662374979352251, 0.05543866794826918, 0.11350784708500211, 0.45429869056991656])
-    #initial = sampler.random(n=number_of_rings + 1).flatten() * eta  # 0からetaの範囲でスケーリング
+    sampler = LatinHypercube(d=number_of_rings+1)  
+    samples = sampler.random(n=1)  # 1サンプルだけ生成（shape: (1, number_of_rings + 1)）
+    #initial = np.array([0.47861465815127613, 0.059825303636854066, 0.02833346559136013, 0.03662374979352251, 0.05543866794826918, 0.11350784708500211, 0.45429869056991656])
+    initial = sampler.random(n=number_of_rings + 1).flatten() * eta  # 0からetaの範囲でスケーリング
     popsize = 4 + math.floor(3 * math.log(number_of_rings+1)) + 8
     #sigma = 0.3*(number_of_rings+1)/9
     sigma = 0.07
