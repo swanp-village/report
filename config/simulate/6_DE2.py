9ba3b41b04c27408d59b7f12784b0b6cc2e7dea3
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-02-17 13:35:38 +0900

    Update 6_DE2.py

--- config/simulate/6_DE2.py
+++ config/simulate/6_DE2.py
@@ -23,6 +23,6 @@ config = {
     "n_eff": 2.2,
     "n_g": 4.4,
     "center_wavelength": 1.55e-06,
-    
-    "label":"CMA_+_error"
+    "label":"CMA_+_error",
+    "lambda_limit": np.arange(1520e-9, 1560e-9, 1e-12)
 }
