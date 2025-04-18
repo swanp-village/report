9d8125c895f9d78e5d5dd10b60bb3843d2fbda80
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-04-18 19:56:42 +0900

    Update evaluator.py

--- MRR/evaluator.py
+++ MRR/evaluator.py
@@ -186,10 +186,12 @@ def _evaluate_ripple(
     three_db_band = pass_band[index[0] : index[-1]]
     maxid = argrelmax(three_db_band, order=1)
     minid = argrelmin(three_db_band, order=1)
-    peak_max = three_db_band[maxid]
-    peak_min = three_db_band[minid]
-    if len(peak_min) == 0:
-        return (1, True)
+    if len(minid[0])==0 or len(maxid[0])==0
+        peak_max = three_db_band.max()
+        peak_min = three_db_band.min()
+    else:
+        peak_max = three_db_band[maxid].max()
+        peak_min = three_db_band[minid].min()
 
     dif = peak_max.max() - peak_min.min()
     if dif > r_max:
