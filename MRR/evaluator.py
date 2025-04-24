77d871e6f4323fbe9fb5e1ae759563513c7bc888
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-04-24 22:54:47 +0900

    Update evaluator.py

--- MRR/evaluator.py
+++ MRR/evaluator.py
@@ -107,7 +107,7 @@ def _evaluate_pass_band(
     x: npt.NDArray[np.float_], y: npt.NDArray[np.float_], H_p: float, start: int, end: int
 ) -> tuple[np.float_, bool]:
     distance: np.float_ = x[1] - x[0]
-    a = max(abs(H_p * (x[end] - x[start],1e-6)
+    a = max(abs(H_p * (x[end] - x[start],1e-6))
     b = abs(np.sum(H_p - y[start:end]) * distance)
     E = b / a
     if abs(H_p * (x[end] - x[start]) < 1e-6
