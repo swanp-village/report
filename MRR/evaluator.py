513e4990680f6f0b493f9c0b332ea9cfb256b587
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-07-04 13:35:31 +0900

    Update evaluator.py

--- MRR/evaluator.py
+++ MRR/evaluator.py
@@ -298,7 +298,7 @@ def _evaluate_cross_talk(
         return (np.float_(0), False)
     return (np.float_(0), True)
 
-
+"""
 def _evaluate_cross_talk(
     y: npt.NDArray[np.float_], max_crosstalk: float, pass_band_start: int, pass_band_end: int
 ) -> tuple[np.float_, bool]:
@@ -334,11 +334,11 @@ def _evaluate_cross_talk(
         # あるいは、r_max のように許容超過量を設定する
         E = 1 / (1 + total_excess)
     return (E, True) # 超過があればFalse
-    """
+    
     else:
         E = 1.0
         return(E, True)
-    """
+"""
 
 def _evaluate_shape_factor(
     x: npt.NDArray[np.float_], y: npt.NDArray[np.float_], start: int, end: int
