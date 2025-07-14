688057f1c1d53492e3b4963278fc0a5624347d04
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-07-14 13:54:21 +0900

    Update simulator.py

--- MRR/simulator.py
+++ MRR/simulator.py
@@ -75,10 +75,10 @@ def simulate_MRR(
         wavelength=x, L=L, K=K, alpha=alpha, eta=eta, n_eff=n_eff, n_g=n_g, center_wavelength=center_wavelength
     )
    
-    if skip_evaluation:
-        evaluation_result = np.float_(0)
-    else:
-        evaluation_result = evaluate_band(
+    #if skip_evaluation:
+        #evaluation_result = np.float_(0)
+    
+    evaluation_result = evaluate_band(
             x=x,
             y=y,
             center_wavelength=center_wavelength,
@@ -90,7 +90,7 @@ def simulate_MRR(
             r_max=r_max,
             weight=weight,
             ignore_binary_evaluation=ignore_binary_evaluation,
-        )
+    )
         
       
     accumulator.logger.print_parameters(K=K, L=L, N=N, FSR=practical_FSR, E=evaluation_result, format=format)
