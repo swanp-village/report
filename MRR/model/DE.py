a30dda727fc242e3b7d76d37dfac190a844682be
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-07-15 12:54:38 +0900

    Update DE.py

--- MRR/model/DE.py
+++ MRR/model/DE.py
@@ -189,7 +189,7 @@ def optimize_K(
                 for _ in range(num_starts)]
 
     with ProcessPoolExecutor() as executor:
-        futures = [executor.submit(cma_run_sigma, initial, bounds_array, popsize, sigma, generations, params)
+        futures = [executor.submit(cma_run, initial, bounds_array, popsize, sigma, generations, params)
                    for initial in initials]
 
         results = [f.result() for f in futures]
