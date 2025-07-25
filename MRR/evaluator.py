2bbf5c1c94776a6c10be2f6e8c68d3d271b1febf
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-07-25 23:46:44 +0900

    Update evaluator.py

--- MRR/evaluator.py
+++ MRR/evaluator.py
@@ -296,8 +296,11 @@ def _evaluate_cross_talk(  y: npt.NDArray[np.float_], max_crosstalk: float, pass
     score = np.sum(excess_start) + np.sum(excess_end)
     a = np.any(start_peak > max_crosstalk)
     b = np.any(end_peak > max_crosstalk)
+    E = １ / (1 + np.exp(score - 60)
+    """
     normalized_score = np.log(score + 1)
     E = 1 - np.exp(-normalized_score/4)
+    """
     if a or b:
         return(E, False)
     else:
