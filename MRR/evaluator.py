fa30bba2d20de4820f36b8ec4e2464a8eb867c16
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-07-25 13:55:09 +0900

    Update evaluator.py

--- MRR/evaluator.py
+++ MRR/evaluator.py
@@ -172,11 +172,12 @@ def _evaluate_insertion_loss(
     insertion_loss_at_center = y[idx] # これで単一の数値が得られる
     loss_abs = np.abs(insertion_loss_at_center)
     H_i_abs = np.abs(H_i)
-    E = 1 / (1 + np.exp(loss_abs - H_i_abs)
+    E = 1 / (1 + np.exp(loss_abs - H_i_abs))
     if insertion_loss_at_center < H_i:
         return(E, False)
     else:
         return(E, True)
+             
 
 
 
