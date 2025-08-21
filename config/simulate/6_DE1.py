9241b9647e9215a4ce60d50d6c1538a4e381b7e8
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-08-21 14:19:00 +0900

    Update 6_DE1.py

--- config/simulate/6_DE1.py
+++ config/simulate/6_DE1.py
@@ -25,5 +25,5 @@ config = {
     "n_g": 4.4,
     "center_wavelength": 1.55e-06,
     "label":"CMAES",
-    "lambda_limit": np.arange(1540e-9, 1560e-9, 1e-12)
+    "lambda_limit": np.arange(1520e-9, 1560e-9, 1e-12)
 }
