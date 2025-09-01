12dbbbb288fc508a47234041a69f888e2f43fa5a
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-09-01 16:14:19 +0900

    Update 6_DE2.py

--- config/simulate/6_DE2.py
+++ config/simulate/6_DE2.py
@@ -23,6 +23,6 @@ config = {
     "n_eff": 2.2,
     "n_g": 4.4,
     "center_wavelength": 1.55e-06,
-    "label":"after",
-    "lambda_limit": np.arange(1520e-9, 1560e-9, 1e-12)
+    "label":"before",
+    "lambda_limit": np.arange(1540e-9, 1560e-9, 1e-12)
 }
