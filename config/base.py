6d46e47ad0b1b22b4689e5011ab548e00c6f1d4d
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-09-01 12:29:25 +0900

    Update base.py

--- config/base.py
+++ config/base.py
@@ -2,7 +2,7 @@ from config.model import OptimizationConfig
 
 config = OptimizationConfig(
     eta=0.996,
-    alpha=13.82,
+    alpha=52.96,
     n_eff=2.2,
     n_g=4.4,
     number_of_rings=6,
@@ -16,6 +16,6 @@ config = OptimizationConfig(
     r_max=5,
     weight=[1.0, 3.5, 1.0, 5.0, 3.5, 1.0, 1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
     min_ring_length=50e-6,
-    number_of_generations=1,
+    number_of_generations=100,
     strategy=[0.03, 0.07, 0.2, 0.7],
 )
