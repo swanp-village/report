e7dfb72bcb7829d2becb9a72cc29c41316d924d5
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-10-10 13:51:36 +0900

    Update DE.py

--- MRR/model/DE.py
+++ MRR/model/DE.py
@@ -103,12 +103,13 @@ def cma_run(initial, bounds_array, popsize, sigma, generations, params,objective
         candidates = es.ask()
         fitnesses = [objective_func(x) for x in candidates]
         es.tell(candidates, fitnesses)
-        
+        """
         if es.sigma < 0.1:
             es.sigma = 0.1
         
         elif es.sigma > 0.7:
             es.sigma = 0.7
+        """
 
         min_fit = min(fitnesses)
         if min_fit < best_fitness:
