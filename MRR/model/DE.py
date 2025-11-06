8a91e52eb6d30d002b82fc0f27bc866d1e8dbe68
swanp-village <92138575+swanp-village@users.noreply.github.com>
2025-11-06 13:38:05 +0900

    Update DE.py

--- MRR/model/DE.py
+++ MRR/model/DE.py
@@ -279,7 +279,7 @@ def optimize_K(
         # データセットを更新
         X_train.append(acq_best_K)
         Y_train.append(true_fitness_new)
-        
+        print(f"今回の評価値",true_fitness_new)
         # 全体の最良解を更新
         if true_fitness_new < best_fitness:
             best_fitness = true_fitness_new
