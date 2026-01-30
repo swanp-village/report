f4d55f66a27f498a07f7351d3dd6ca8f5d6a0d2a
swanp-village <92138575+swanp-village@users.noreply.github.com>
2026-01-30 15:42:09 +0900

    Update DE.py

--- MRR/model/DE.py
+++ MRR/model/DE.py
@@ -263,8 +263,8 @@ def optimize_K(
     bounds = [(1e-12, eta) for _ in range(number_of_rings + 1)]
     bounds_array=np.array(bounds) 
     popsize = 4 + math.floor(3 * math.log(number_of_rings+1)) + 8
-    sigma = 0.5
-    generations = 500
+    sigma = 0.3
+    generations = 300
     num_starts = 6
     initials = [rng.uniform(1e-12, eta, size=(number_of_rings + 1,))
                 for _ in range(num_starts)]
