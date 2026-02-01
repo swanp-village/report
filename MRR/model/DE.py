28ff88c2a70301a766076ebbb47dea66708c38d7
swanp-village <92138575+swanp-village@users.noreply.github.com>
2026-02-01 14:44:45 +0900

    Update DE.py

--- MRR/model/DE.py
+++ MRR/model/DE.py
@@ -249,7 +249,7 @@ def SHACMA_run(initial, bounds_array, popsize, sigma, generations, params):
         #print("記録メモリ sigma = ",mem_sigma)
         #print("記録メモリ ccov = ",mem_ccov)
         if best_fitness > -11.0:
-            if stagnation_counter > 30:
+            if stagnation_counter > 100:
                 print("探索をやり直します")
                 if len(archive) > 0:
                     base_point = random.choice(list(archive))
