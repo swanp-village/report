2f4358e06c43fb60527f4a883188bd5a48be037a
swanp-village <92138575+swanp-village@users.noreply.github.com>
2026-03-06 10:57:47 +0900

    Update DE.py

--- MRR/model/DE.py
+++ MRR/model/DE.py
@@ -635,8 +635,6 @@ def optimize_perturbed_K_func(K: npt.NDArray[np.float_], params: OptimizeKParams
         ignore_binary_evaluation=False,
     )
 
-    return E_positive
-"""
     # 負の誤差での評価値
     y_negative = simulate_transfer_function(
         wavelength=x,
@@ -661,7 +659,7 @@ def optimize_perturbed_K_func(K: npt.NDArray[np.float_], params: OptimizeKParams
         weight=params.weight,
         ignore_binary_evaluation=False,
     )
- """
+    return E_positive,E_negative
 
     
 
